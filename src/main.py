c37262c4ce5f0c5fddbafe88b4c1dc35fda3da6e
nickscript0 <nickscript0@users.noreply.github.com>
2015-09-12 14:14:41 -0600

    working query with gha todojs frameworks

--- src/main.py
+++ src/main.py
@@ -6,6 +6,27 @@ from apiclient.errors import HttpError
 
 from oauth2client.client import GoogleCredentials
 
+QUERY1 = """
+SELECT repository_url, COUNT(*) as star_count,
+  STRFTIME_UTC_USEC(created_at, '%m') as month,
+  STRFTIME_UTC_USEC(created_at, '%Y') as year
+  FROM (
+  TABLE_QUERY([githubarchive:month],
+    'REGEXP_MATCH(table_id, r"^201[1-4][01]\d")'
+
+  ))
+WHERE type = 'WatchEvent' AND
+  repository_url in ('https://github.com/angular/angular.js', 'https://github.com/emberjs/ember.js', 'https://github.com/lhorie/mithril.js', 'https://github.com/facebook/react', 'https://github.com/aurelia/framework', 'https://github.com/jashkenas/backbone', 'https://github.com/kmalakoff/knockback', 'https://github.com/meteor/meteor', 'https://github.com/ampersandjs/ampersand', 'https://github.com/leaddyno/intercooler-js', 'https://github.com/bluespire/durandal', 'https://github.com/deftsjs/deftjs', 'https://github.com/rappid/rappid.js', 'https://github.com/angular/angular.dart', 'https://github.com/componentjs/component', 'https://github.com/chaplinjs/chaplin', 'https://github.com/flams/olives', 'https://github.com/elabs/serenade.js', 'https://github.com/marionettejs/backbone.marionette', 'https://github.com/rhysbrettbowen/plastronjs', 'https://github.com/knockout/knockout', 'https://github.com/atmajs/maskjs', 'https://github.com/mutualmobile/lavaca', 'https://github.com/epitome-mvc/epitome', 'https://github.com/enyojs/enyo', 'https://github.com/quirkey/sammy', 'https://github.com/ariatemplates/ariatemplates', 'https://github.com/jquery/jquery', 'https://github.com/meteor/meteor', 'https://github.com/jashkenas/backbone', 'https://github.com/facebook/react', 'https://github.com/polymer/polymer', 'https://github.com/flightjs/flight', 'https://github.com/derbyjs/derby', 'https://github.com/ractivejs/ractive', 'https://github.com/socketstream/socketstream', 'https://github.com/yyx990803/vue', 'https://github.com/spine/spine', 'https://github.com/yui/yui3', 'https://github.com/lhorie/mithril.js', 'https://github.com/batmanjs/batman', 'https://github.com/elm-lang/elm-compiler', 'https://github.com/walmartlabs/thorax', 'https://github.com/montagejs/montage', 'https://github.com/telerik/kendo-ui-core', 'https://github.com/bitovi/canjs', 'https://github.com/kmalakoff/knockback', 'https://github.com/google/closure-compiler', 'https://github.com/paulmillr/exoskeleton', 'https://github.com/ampersandjs/ampersand', 'https://github.com/gwtproject/gwt', 'https://github.com/dojo/dojo', 'https://github.com/hay/stapes', 'https://github.com/somajs/somajs', 'https://github.com/firebase/angularfire', 'https://github.com/mozart/mozart2', 'https://github.com/pathikrit/dijon')
+GROUP BY repository_url, month, year
+ORDER BY year, month, star_count DESC;
+"""
+
+QUERY_ORIG = """
+SELECT TOP(corpus, 10) as title,
+          COUNT(*) as unique_words
+          FROM [publicdata:samples.shakespeare];
+"""
+
 
 def main(project_id):
     # [START build_service]
@@ -19,9 +40,7 @@ def main(project_id):
         # [START run_query]
         query_request = bigquery_service.jobs()
         query_data = {
-            'query': ('SELECT TOP(corpus, 10) as title, '
-                      'COUNT(*) as unique_words '
-                      'FROM [publicdata:samples.shakespeare];')
+            'query': (QUERY1)
         }
 
         query_response = query_request.query(
